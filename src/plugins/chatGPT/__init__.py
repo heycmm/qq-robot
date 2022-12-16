bbe8d88dba2bf417f461d69c702a9260f1fedf79
czx.me <heycmm.cn@gmail.com>
2022-12-16 19:49:37 +0800

    :bug:chatGPT

--- src/plugins/chatGPT/__init__.py
+++ src/plugins/chatGPT/__init__.py
@@ -19,14 +19,18 @@ async def _say(bot: Bot, event: Event, state: T_State):
     msg = str(event.get_message()).strip()
     if msg:
         msg = msg.split()
-        url_tts = chat_api + parse.quote(msg[1].encode('utf-8'))
-        result = await get_random_img(url_tts)
+        url_chat = chat_api + parse.quote(msg[1].encode('utf-8'))
+        result = await get_chat_gpt(url_chat)
         await chat.send(result)
     logger.info(
         f"USER {event.get_user_id()} GROUP "
         f"{event.group_id if isinstance(event, GroupMessageEvent) else 'private'} chat： {msg}"
     )
 
-async def get_random_img(url_tts):
-    data = requests.get(url_tts)
-    return data.url
+
+async def get_chat_gpt(url_chat):
+    data = requests.get(url_chat)
+    if data.status_code == 200:
+        return data.text
+    else:
+        return '服务挂了'
